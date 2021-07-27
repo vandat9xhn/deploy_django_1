f24fa1cfaed58a4fd746c922b1722efedcc47d29
Nguyen Dat <vandat9xiloveyou@gmail.com>
2021-07-27 16:50:13 +0700

    CORS allowed all

--- react_django_1/settings.py
+++ react_django_1/settings.py
@@ -72,9 +72,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8000",
+# ]
 
 # CORS_ORIGIN_WHITELIST = (
 #     "http://localhost:8000",
